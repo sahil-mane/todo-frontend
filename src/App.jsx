cc33a9b1ab85033a6e32ef9ac3ea8fd8b985743d
shravan <shravanmane216@gmail.com>
2024-12-13 00:48:13 +0530

    my third commit in 13/12/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import image from "./assets/images.jpeg"
+import { Route, Routes } from "react-router-dom"
+import Login from "./pages/login/Login"
+import Signup from "./pages/signup/Signup"
+import Dashboard from "./pages/dashboard/Dashboard"
 function App() {
 
   return (
     <>
-      <span className="flex justify-center bg-red-300 border">SAHIL MANE</span>
+      {/* <span className="flex justify-center bg-red-300 border">SAHIL MANE</span>
 
       <img className="w-10 h-10 rounded-full" src={image} alt="Rounded avatar" />
       <img className="w-10 h-10 rounded" src={image} alt="Default avatar" />
-      <h1>sahi hai bhai 2</h1>
+      <h1>sahi hai bhai 2</h1> */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
 
     </>
   )
